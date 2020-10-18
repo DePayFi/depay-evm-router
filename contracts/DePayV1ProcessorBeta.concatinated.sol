62956c84e87ef39d2ec19e1f942d2e826628b739
Sebastian Pape <s@pape.io>
2020-10-18 12:42:22 +0200

    working payment processor

--- contracts/DePayV1ProcessorBeta.concatinated.sol
+++ contracts/DePayV1ProcessorBeta.concatinated.sol
@@ -815,47 +815,53 @@ contract Ownable is Context {
 
 interface IDePayV1ProcessorBeta {
     
-    event ApproveAllDebug(
-        address[] path
+    event BalanceDebug(
+        address[] path,
+        uint balanceBefore,
+        uint balanceAfter
     );
     
-    event SwapDebug(
-        address[] path,
-        uint amountIn,
-        uint amountOut
-    );
-  
     event Payment(
         uint indexed id,
         address indexed sender,
-        address payable indexed receiver,
-        uint context
+        address payable indexed receiver
     );
-  
+
    function pay(
         address[] calldata path,
         uint amountIn,
         uint amountOut,
-        uint routerId,
         address payable receiver,
-        uint id,
-        uint context
+        uint id
     ) external payable;
-  
+
 }
 
-interface IDePayV1RouterBeta {
-  
-    function swap(
-        address[] calldata path,
+interface IUniswapV2Router01 {
+    
+    function swapExactTokensForTokens(
         uint amountIn,
-        uint amountOut
-    ) external;
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
     
-    function contractAddress() external view returns(address);
+    function swapExactTokensForETH(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+    uint deadline
+    ) external returns (uint[] memory amounts);
+        
+    function swapExactETHForTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint[] memory amounts);
     
-    function requiresApproval(address[] calldata path) external view returns(address[] memory);
-  
 }
 
 contract DePayV1ProcessorBeta is IDePayV1ProcessorBeta, Ownable, ReentrancyGuard {
@@ -863,19 +869,20 @@ contract DePayV1ProcessorBeta is IDePayV1ProcessorBeta, Ownable, ReentrancyGuard
     address[] public routers;
     address private ZERO = 0x0000000000000000000000000000000000000000;
     uint private MAXINT = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+    address public UniswapV2Router02 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address public WETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
     
-     function allRouters() external view returns(address[] memory) {
-        return routers;
+    receive() external payable {
+        // accepts eth payments which are required to
+        // swap and pay from ETH to any token
     }
-    
+
     function pay(
         address[] calldata path,
         uint amountIn,
         uint amountOut,
-        uint routerId,
         address payable receiver,
-        uint id,
-        uint context
+        uint id
     ) external payable override nonReentrant {
         if(path[0] == ZERO) { require(msg.value >= amountIn, 'DePay: Insufficient amount payed in.'); }
         
@@ -883,11 +890,11 @@ contract DePayV1ProcessorBeta is IDePayV1ProcessorBeta, Ownable, ReentrancyGuard
             _pay(receiver, msg.sender, path[0], amountOut);
         } else {
             transferIn(path[0], amountIn);
-            //swap(routerId, path, amountIn, amountOut);
+            swap(path, amountIn, amountOut);
             _pay(receiver, address(this), path[path.length-1], amountOut);
         }
         
-        emit Payment(id, msg.sender, receiver, context);
+        emit Payment(id, msg.sender, receiver);
     }
     
     function transferIn(address token, uint amount) private {
@@ -896,28 +903,64 @@ contract DePayV1ProcessorBeta is IDePayV1ProcessorBeta, Ownable, ReentrancyGuard
         }
     }
     
-    function swap(uint routerId, address[] memory path, uint amountIn, uint amountOut) private {
+    function swap(address[] memory path, uint amountIn, uint amountOut) private {
         uint balanceBefore = balance(path[path.length-1]);
-        
-        approveAll(path, IDePayV1RouterBeta(routers[routerId]).contractAddress());
-        
-        // routers[routerId].delegatecall(abi.encodeWithSignature("swap(address[],uint256, uint256)", path, amountIn, amountOut));
-
-        uint balanceAfter = balance(path[path.length-1]);
-        // require(balanceAfter > (balanceBefore + amountOut), 'DePay: Insufficient balance after swap.');
+        swapOnUniswap(path, amountIn, amountOut);
+        require(balance(path[path.length-1]) >= (balanceBefore + amountOut), 'DePay: Insufficient balance after swap.');
     }
     
-    function approveAll(address[] memory tokens, address contractAddress) private {
-        for (uint i; i < tokens.length - 1; i++) {
-            ERC20(tokens[i]).approve(contractAddress, MAXINT);
+    function swapOnUniswap(address[] memory path, uint amountIn, uint amountOut) private {
+        address[] memory uniPath = new address[](path.length);
+        for (uint i=0; i<path.length; i++) {
+            if(path[i] == ZERO) {
+                uniPath[i] = WETH;
+            } else {
+                uniPath[i] = path[i];
+            }
         }
+
+        if( 
+            path[0] != ZERO &&
+            ERC20(path[0]).allowance(address(this), UniswapV2Router02) < amountIn 
+        ) {
+            ERC20(path[0]).approve(UniswapV2Router02, MAXINT);
+        }
+
+        if(path[0] == ZERO) {
+            IUniswapV2Router01(UniswapV2Router02).swapExactETHForTokens{value: amountIn}(
+                amountOut,
+                uniPath,
+                address(this),
+                swapDeadline()
+            );
+        } else if (path[path.length-1] == ZERO) {
+            IUniswapV2Router01(UniswapV2Router02).swapExactTokensForETH(
+                amountIn,
+                amountOut,
+                uniPath,
+                address(this),
+                swapDeadline()
+            );
+        } else {
+            IUniswapV2Router01(UniswapV2Router02).swapExactTokensForTokens(
+                amountIn,
+                amountOut,
+                uniPath,
+                address(this),
+                swapDeadline()
+            );
+        }
+    }
+    
+    function swapDeadline() private view returns(uint) {
+        return block.timestamp + 60 minutes;
     }
     
     function balance(address token) private view returns(uint) {
         if(token == ZERO) {
             return address(this).balance;
         } else {
-            ERC20(token).balanceOf(address(this));
+            return ERC20(token).balanceOf(address(this));
         }
     }
     
@@ -932,11 +975,7 @@ contract DePayV1ProcessorBeta is IDePayV1ProcessorBeta, Ownable, ReentrancyGuard
             }
         }
     }
-    
-    function addRouter(address routerAddress) external onlyOwner {
-        routers.push(routerAddress);
-    }
-    
+
     function payableOwner() view private returns(address payable) {
         return address(uint160(owner()));
     }
