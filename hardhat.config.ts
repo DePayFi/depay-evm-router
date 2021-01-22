e6e0fdc8003f16f9493fb2571bd959c79baa8caa
Sebastian Pape <s@pape.io>
2021-01-22 17:37:34 +0100

    preparing contract call processor

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,10 @@ const config: HardhatUserConfig = {
     compilers: [
       { version: "0.7.5", settings: {} }, // for DePay
       { version: "0.5.16", settings: {} }, // for Uniswap
+      { version: "0.6.0", settings: {} }, // for StakingPool
+      { version: "0.6.2", settings: {} }, // for StakingPool
       { version: "0.6.6", settings: {} }, // for Uniswap
+      { version: "0.6.12", settings: {} }, // for StakingPool
       { version: "0.4.18", settings: {} } // for WETH
     ],
   }
