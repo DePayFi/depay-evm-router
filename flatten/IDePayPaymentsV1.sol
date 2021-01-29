00c897f2d34d91262e1be963da017406f8a53b88
Sebastian Pape <s@pape.io>
2021-01-29 18:41:44 +0100

    update flattened contracts

---
+++ flatten/IDePayPaymentsV1.sol
@@ -0,0 +1,44 @@
+// Root file: contracts/interfaces/IDePayPaymentsV1.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.5 <0.8.0;
+pragma abicoder v2;
+
+interface IDePayPaymentsV1 {
+
+  event Payment(
+    address indexed sender,
+    address payable indexed receiver
+  );
+
+  event PluginApproved(
+    address indexed pluginAddress
+  );
+
+  function ZERO() external view returns(address);
+
+  function approvedPlugins(address) external view returns(address);
+
+  function pay(
+    address[] calldata path,
+    uint[] calldata amounts,
+    address[] calldata addresses,
+    address[] calldata plugins,
+    string[] calldata data
+  ) external payable returns(bool);
+
+  function approvePlugin(
+    address plugin
+  ) external returns(bool);
+
+  function isApproved(
+    address pluginAddress
+  ) external view returns(bool);
+
+  function withdraw(
+    address token,
+    uint amount
+  ) external returns(bool);
+  
+}
---
+++ flatten/IDePayPaymentsV1Plugin.sol
@@ -0,0 +1,16 @@
+// Root file: contracts/interfaces/IDePayPaymentsV1Plugin.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.5 <0.8.0;
+pragma abicoder v2;
+
+interface IDePayPaymentsV1Plugin {
+
+  function execute(
+    address[] calldata path,
+    uint[] calldata amounts,
+    address[] calldata addresses,
+    string[] calldata data
+  ) external payable returns(bool);
+}
