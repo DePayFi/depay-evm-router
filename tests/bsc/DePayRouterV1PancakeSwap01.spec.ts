91fa2b32d0bfe1e4eaf15729cbd9f8e8a985f6b2
Sebastian Pape <s@pape.io>
2021-08-05 08:18:12 +0200

    finish pancakeswap plugin tests

--- tests/bsc/DePayRouterV1PancakeSwap01.spec.ts
+++ tests/bsc/DePayRouterV1PancakeSwap01.spec.ts
@@ -1,5 +1,6 @@
 import deployConfiguration from '../helpers/deploy/configuration'
 import deployRouter from '../helpers/deploy/router'
+import impersonate from '../helpers/impersonate'
 import IPancakeRouter02 from '../../artifacts/contracts/interfaces/IPancakeRouter02.sol/IPancakeRouter02.json'
 import now from '../helpers/now'
 import { ethers } from 'hardhat'
@@ -14,6 +15,7 @@ describe('DePayRouterV1PancakeSwap01 on BSC', function() {
   let exchange = findByName('pancakeswap')
 
   let BUSD = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
+  let CAKE = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82'
 
   let ownerWallet,
       otherWallet,
@@ -67,10 +69,45 @@ describe('DePayRouterV1PancakeSwap01 on BSC', function() {
   })
 
   it('swaps BUSD to ETH and performs payment with ETH', async () => {
-    
+    let amountIn = 1000
+    let PancakeRouter = await ethers.getContractAt(IPancakeRouter02.abi, exchange.contracts.router.address)
+    let amountsOut = await PancakeRouter.getAmountsOut(amountIn, [BUSD, CONSTANTS.bsc.WRAPPED])
+    let amountOutMin = amountsOut[amountsOut.length-1].toString()
+    let BUSDToken = await ethers.getContractAt(Token.bsc.BEP20, BUSD)
+    const addressWithBUSD = '0x7Cc3964F0eBc218b6fFb374f9Dad7464e2Cb81C8'
+    const signer = await impersonate(addressWithBUSD)
+    await BUSDToken.connect(signer).approve(router.address, CONSTANTS.bsc.MAXINT)
+    let allowance = await BUSDToken.connect(signer).allowance(addressWithBUSD, router.address)
+    await expect(() => 
+      router.connect(signer).route(
+        [BUSD, CONSTANTS.bsc.NATIVE], // path
+        [amountIn, amountOutMin, now()+60000], // amounts
+        [addressWithBUSD, otherWallet.address], // addresses
+        [swapPlugin.address, paymentPlugin.address], // plugins
+        [] // data
+      )
+    ).to.changeEtherBalance(otherWallet, amountOutMin)
   })
 
-  it('swaps ETH to BUSD and performs payment with BUSD', async () => {
-
+  it('swaps BUSD to CAKE and performs payment with CAKE', async () => {
+    let amountIn = 1000
+    let PancakeRouter = await ethers.getContractAt(IPancakeRouter02.abi, exchange.contracts.router.address)
+    let amountsOut = await PancakeRouter.getAmountsOut(amountIn, [BUSD, CONSTANTS.bsc.WRAPPED, CAKE])
+    let amountOutMin = amountsOut[amountsOut.length-1].toString()
+    let BUSDToken = await ethers.getContractAt(Token.bsc.BEP20, BUSD)
+    let CAKEToken = await ethers.getContractAt(Token.bsc.BEP20, CAKE)
+    const addressWithBUSD = '0x7Cc3964F0eBc218b6fFb374f9Dad7464e2Cb81C8'
+    const signer = await impersonate(addressWithBUSD)
+    await BUSDToken.connect(signer).approve(router.address, CONSTANTS.bsc.MAXINT)
+    let allowance = await BUSDToken.connect(signer).allowance(addressWithBUSD, router.address)
+    await expect(() => 
+      router.connect(signer).route(
+        [BUSD, CONSTANTS.bsc.NATIVE, CAKE], // path
+        [amountIn, amountOutMin, now()+60000], // amounts
+        [addressWithBUSD, otherWallet.address], // addresses
+        [swapPlugin.address, paymentPlugin.address], // plugins
+        [] // data
+      )
+    ).to.changeTokenBalance(CAKEToken, otherWallet, amountOutMin)
   })
 })
