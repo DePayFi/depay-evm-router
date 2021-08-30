dd064e6d603fbecaa95077308747e08f3e5ccbb8
Sebastian Pape <s@pape.io>
2021-08-30 15:50:42 +0200

    fix github workflow/hardhat config

--- hardhat.config.bsc.ts
+++ hardhat.config.bsc.ts
@@ -8,9 +8,6 @@ const hardhatConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
-      blockGasLimit: 12500000,
-      gas: 6500000,
-      gasPrice: 2000000000,
       accounts: {
         mnemonic: MNEMONIC!
       },
