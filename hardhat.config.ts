491949466f8d00fbdb0c586f3f4b87d360ad289f
Sebastian Pape <s@pape.io>
2021-08-30 14:33:27 +0200

    fix hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,13 @@ import { HardhatUserConfig } from 'hardhat/types'
 import { config } from './hardhat.config.shared'
 
 const hardhatConfig: HardhatUserConfig = {
-  ...config
+  ...config,
+  hardhat: {
+    blockGasLimit: 12500000,
+    gas: 6500000,
+    gasPrice: 2000000000,
+    hardfork: 'berlin'
+  }
 }
 
 export default hardhatConfig
