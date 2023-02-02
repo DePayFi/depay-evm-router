320a3d3219b0d0d39d46d8eb8c168d74afb17e50
shujip <shujip@163.com>
2023-02-02 22:50:24 +0800

    add memocontract plugin

---
+++ contracts/interfaces/IMemoworldNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.6 <0.9.0;
+
+interface IMemoworldNFT {
+  function mint(
+    address account,
+    uint256 id,
+    uint256 amount,
+    string memory data
+  ) external payable;
+}
