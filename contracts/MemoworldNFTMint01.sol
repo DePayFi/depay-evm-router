c6822589062803227e48c046a511ca7a7e686f51
shujip <33785030+shujip@users.noreply.github.com>
2023-02-13 16:55:08 +0800

    add memocontract plugin (#50)
    * add memocontract plugin
    
    * update for pr

---
+++ contracts/MemoworldNFTMint01.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.6 <0.9.0;
+pragma abicoder v2;
+
+import {IMemoworldNFT} from './interfaces/IMemoworldNFT.sol';
+
+contract MemoworldNFTMint01 {
+  // Address representating ETH (e.g. in payment routing paths)
+  address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+  // Indicates that this plugin requires delegate call
+  bool public immutable delegate = true;
+  address public constant MemoworldContract = 0x4908b9381606C34EaF4ea24C01Eb66CBAB2bc749;
+
+  event RouteMint(address indexed sender, uint256 indexed tokenId, uint256 indexed amount);
+
+  constructor() {}
+
+  function execute(
+    address[] calldata path,
+    uint256[] calldata amounts,
+    address[] calldata addresses,
+    string[] calldata data
+  ) external payable returns (bool) {
+    IMemoworldNFT memoContract = IMemoworldNFT(MemoworldContract);
+
+    memoContract.mint{value: amounts[1]}(addresses[0], amounts[5], amounts[6], bytes(''));
+
+    emit RouteMint(addresses[0], amounts[5], amounts[6]);
+
+    return true;
+  }
+}
