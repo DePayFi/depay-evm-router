e6a92ca2627b9dc3f797bee3e76359a71aa134e6
Sebastian Pape <s@pape.io>
2023-08-07 11:52:54 +0200

    fix tests

--- test/gnosis/pay_with_token.spec.ts
+++ test/gnosis/pay_with_token.spec.ts
@@ -3,6 +3,6 @@ import payWithToken from '../_pay-with-token'
 payWithToken({
   blockchain: 'gnosis',
   token: '0x4ECaBa5870353805a9F068101A40E0f32ed605C6', // USDT
-  tokenHolder: '0x6c5bb1514f2f8f0256cbb4570860f90b288e29f4',
+  tokenHolder: '0x5bb83e95f63217cda6ae3d181ba580ef377d2109',
   tokenReversalReason: 'SafeERC20: low-level call failed',
 })
