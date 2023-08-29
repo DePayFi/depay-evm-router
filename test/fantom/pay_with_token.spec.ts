7af32100f249d82306a6fcf3cc88541c39a98a2a
Sebastian Pape <s@pape.io>
2023-08-29 12:35:55 +0200

    fix fantom

--- test/fantom/pay_with_token.spec.ts
+++ test/fantom/pay_with_token.spec.ts
@@ -4,5 +4,5 @@ payWithToken({
   blockchain: 'fantom',
   token: '0xdc301622e621166BD8E82f2cA0A26c13Ad0BE355', // FRAX
   fromAccount: '0xd6e2F33B33271E97c4011218b88e334448818689',
-  reversalReason: 'WERC10: request exceeds allowance',
+  reversalReason: 'ERC20: transfer amount exceeds allowance',
 })
