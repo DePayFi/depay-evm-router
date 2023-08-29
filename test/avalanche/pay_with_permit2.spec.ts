7686ae050274e3f9612f055ef1cb4d6a988f582f
Sebastian Pape <s@pape.io>
2023-08-29 16:25:29 +0200

    fix avalanche tests

--- test/avalanche/pay_with_permit2.spec.ts
+++ test/avalanche/pay_with_permit2.spec.ts
@@ -3,5 +3,5 @@ import payWithPermit2 from '../_pay-with-permit2'
 payWithPermit2({
   blockchain: 'avalanche',
   token: '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7', // USDT
-  tokenHolder: '0x9f8c163cba728e99993abe7495f06c0a3c8ac8b9',
+  tokenHolder: '0x6B365AF8d060E7F7989985D62485357E34e2e8f5',
 })
