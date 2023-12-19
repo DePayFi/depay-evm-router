b0b196567793d46b31a6a40b9b40cad42adbd020
Sebastian Pape <0xneo11@gmail.com>
2023-12-19 17:22:41 +0100

    fix fantom tests

--- test/fantom/pay_with_exchange_conversion.spec.ts
+++ test/fantom/pay_with_exchange_conversion.spec.ts
@@ -2,8 +2,8 @@ import payWithExchangeConversion from '../_pay-with-exchange-conversion'
 
 payWithExchangeConversion({
   blockchain: 'fantom',
-  fromToken: '0x9879aBDea01a879644185341F7aF7d8343556B7a', // TUSD
-  fromAccount: '0x98bEDd08B149eD859d879930AD007229BA5B18C9', // needs to hold enough fromToken, will be impersonated
+  fromToken: '0x28a92dde19D9989F39A49905d7C9C2FAc7799bDf', // USDC
+  fromAccount: '0xc647CE76ec30033Aa319d472Ae9f4462068f2AD7', // needs to hold enough fromToken, will be impersonated
   toToken: '0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE', // BOO
   exchanges: [
     { name: 'spookyswap', type: 'pull' },
