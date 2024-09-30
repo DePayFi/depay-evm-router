66c4ba8d567ecdb321f1fb47391d42b5b58a4c48
Sebastian Pape <0xneo11@gmail.com>
2024-09-30 10:13:04 +0200

    fix tests

--- test/polygon/pay_with_exchange_conversion.spec.ts
+++ test/polygon/pay_with_exchange_conversion.spec.ts
@@ -2,7 +2,7 @@ import payWithExchangeConversion from '../_pay-with-exchange-conversion'
 
 payWithExchangeConversion({
   blockchain: 'polygon',
-  fromToken: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', // USDT
+  fromToken: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI
   fromAccount: '0xF977814e90dA44bFA03b6295A0616a897441aceC', // needs to hold enough fromToken, will be impersonated
   toToken: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC
   exchanges: [
