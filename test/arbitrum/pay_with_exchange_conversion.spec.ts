7945ca0c4374f11d32463e528211df2ad96b1028
Sebastian Pape <0xneo11@gmail.com>
2024-09-30 10:45:37 +0200

    fix tests

--- test/arbitrum/pay_with_exchange_conversion.spec.ts
+++ test/arbitrum/pay_with_exchange_conversion.spec.ts
@@ -2,8 +2,8 @@ import payWithExchangeConversion from '../_pay-with-exchange-conversion'
 
 payWithExchangeConversion({
   blockchain: 'arbitrum',
-  fromToken: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', // USDT
-  fromAccount: '0xB38e8c17e38363aF6EbdCb3dAE12e0243582891D', // needs to hold enough fromToken, will be impersonated
+  fromToken: '0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0', // UNISWAP
+  fromAccount: '0xe2823659bE02E0F48a4660e4Da008b5E1aBFdF29', // needs to hold enough fromToken, will be impersonated
   toToken: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
   exchanges: [
     { name: 'uniswap_v3', type: 'push' },
--- test/polygon/pay_with_exchange_conversion.spec.ts
+++ test/polygon/pay_with_exchange_conversion.spec.ts
@@ -2,11 +2,10 @@ import payWithExchangeConversion from '../_pay-with-exchange-conversion'
 
 payWithExchangeConversion({
   blockchain: 'polygon',
-  fromToken: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI
-  fromAccount: '0xF977814e90dA44bFA03b6295A0616a897441aceC', // needs to hold enough fromToken, will be impersonated
+  fromToken: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', // WETH
+  fromAccount: '0x62ac55b745F9B08F1a81DCbbE630277095Cf4Be1', // needs to hold enough fromToken, will be impersonated
   toToken: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC
   exchanges: [
     { name: 'uniswap_v3', type: 'push' },
-    { name: 'quickswap', type: 'pull' },
   ]
 })
