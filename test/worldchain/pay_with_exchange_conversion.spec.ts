3a231c64c2c845d1a00ab9117fe8ef845f31bfb0
Sebastian Pape <0xneo11@gmail.com>
2024-11-07 11:37:24 +0100

    fix worldcoin tests

--- test/worldchain/pay_with_exchange_conversion.spec.ts
+++ test/worldchain/pay_with_exchange_conversion.spec.ts
@@ -2,9 +2,9 @@ import payWithExchangeConversion from '../_pay-with-exchange-conversion'
 
 payWithExchangeConversion({
   blockchain: 'worldchain',
-  fromToken: '0x79A02482A880bCE3F13e09Da970dC34db4CD24d1', // USDCe
-  fromAccount: '0xDc927Bd56CF9DfC2e3779C7E3D6d28dA1C219969', // needs to hold enough fromToken, will be impersonated
-  toToken: '0x2cFc85d8E48F8EAB294be644d9E25C3030863003', // WDL
+  fromToken: '0x2cFc85d8E48F8EAB294be644d9E25C3030863003',
+  fromAccount: '0x3b9Ce4B73fB57181194d83EC44544c0ccc77319a', // needs to hold enough fromToken, will be impersonated
+  toToken: '0x79A02482A880bCE3F13e09Da970dC34db4CD24d1',
   exchanges: [
     { name: 'uniswap_v3', type: 'pull' },
   ]
